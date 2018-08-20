75b3cd6bd4e14f7c749d608cddedd3d6adff3eca
chinaten <chinaten2013@gmail.com>
2018-08-20 13:58:59 +0800

    test.go use pkg/linux_386/github.com/stringutil.a

--- hello/test.go
+++ hello/test.go
@@ -1,6 +1,10 @@
 package main
-import "fmt"
+
+import (
+   "fmt"
+   "github.com/stringutil"
+)
+
 func main(){
-   fmt.Printf("Hello,world.\n")
+   fmt.Printf(stringutil.Reverse("!oG,olleH"))
 }
-end
